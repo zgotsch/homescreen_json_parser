8050598734bcd347d15d9f3ad20823e3246d5bea
Zach Gotsch <orbipedis@gmail.com>
2021-11-12 23:20:50 -0800

    Build the thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import prettier from "prettier";
+import parserBabel from "prettier/parser-babel";
+import "./App.css";
+import useGetParamState from "./useGetParamState";
+import {useMemo, useState} from "react";
+import ReactPrismjs from "@uiw/react-prismjs";
+import "prismjs/components/prism-json";
+import "prismjs/themes/prism-tomorrow.css";
 
 function App() {
+  const [value, setValue] = useGetParamState("test");
+  const [error, setError] = useState<null | string>(null);
+
+  const formatted = useMemo(() => {
+    try {
+      const formatted = prettier.format(value, {
+        parser: "json",
+        plugins: [parserBabel],
+      });
+      setError(null);
+      return formatted;
+    } catch (e) {
+      setError((e as any).message);
+      console.log(e);
+      return null;
+    }
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="json-input">
+        <div className="explanation">Paste your JSON here...</div>
+        <textarea value={value} onChange={(e) => setValue(e.target.value)} />
+      </div>
+      <div className="json-output">
+        <div className="explanation">And see it formatted here!</div>
+        {error != null ? (
+          <div className="json-parse-error">
+            <div>JSON parse error!</div>
+            <pre>{error}</pre>
+          </div>
+        ) : null}
+        <ReactPrismjs
+          className="formatted-display"
+          language="json"
+          source={formatted ?? ""}
+        />
+      </div>
     </div>
   );
 }
